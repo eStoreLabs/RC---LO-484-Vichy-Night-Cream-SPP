6db91b066aec55b2b6748b6648373ed975d52b8f
Roman Pavlenko <rpavlenko.mail@gmail.com>
2024-04-22 22:34:42 +0200

    feat: add video, modify script, change cross-3 image

--- 3337875873086/scripts/script.js
+++ 3337875873086/scripts/script.js
@@ -22,29 +22,3 @@ for (let i = 0; i < accTab.length; i++) {
     }
   });
 }
-
-const videoPlayerButton = document.querySelector('.es-video__play-button'),
-  videoPlayer = document.getElementById('player');
-
-videoPlayerButton.addEventListener('click', function () {
-  document.getElementById('player').play();
-  videoPlayerButton.style.display = 'none';
-});
-
-videoPlayer.addEventListener('ended', function () {
-  videoPlayerButton.style.display = 'block';
-});
-
-const videoPlayerButtonDesktop = document.querySelector(
-    '.es-video__play-button--desktop',
-  ),
-  videoPlayerDesktop = document.getElementById('player-desktop');
-
-videoPlayerButtonDesktop.addEventListener('click', function () {
-  document.getElementById('player-desktop').play();
-  videoPlayerButtonDesktop.style.display = 'none';
-});
-
-videoPlayerDesktop.addEventListener('ended', function () {
-  videoPlayerButtonDesktop.style.display = 'block';
-});
